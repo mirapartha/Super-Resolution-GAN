5823dd5de5871b04b6499083e7eee3c23ed9b8a6
Mira Partha <mirapartha@gmail.com>
2021-03-17 01:02:58 -0700

    Added srgan files

---
+++ srgan.py
@@ -0,0 +1,416 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+from torchvision.utils import make_grid
+from torchvision.models import vgg19
+from PIL import Image
+
+class ResidualBlock(nn.Module):
+    '''
+    ResidualBlock Class
+    Values
+        channels: the number of channels throughout the residual block, a scalar
+    '''
+
+    def __init__(self, channels):
+        super().__init__()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+            nn.PReLU(),
+
+            nn.Conv2d(channels, channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(channels),
+        )
+
+    def forward(self, x):
+        return x + self.layers(x)
+
+class Generator(nn.Module):
+    '''
+    Generator Class
+    Values:
+        base_channels: number of channels throughout the generator, a scalar
+        n_ps_blocks: number of PixelShuffle blocks, a scalar
+        n_res_blocks: number of residual blocks, a scalar
+    '''
+
+    def __init__(self, base_channels=64, n_ps_blocks=2, n_res_blocks=16):
+        super().__init__()
+        # Input layer
+        self.in_layer = nn.Sequential(
+            nn.Conv2d(3, base_channels, kernel_size=9, padding=4),
+            nn.PReLU(),
+        )
+
+        # Residual blocks
+        res_blocks = []
+        for _ in range(n_res_blocks):
+            res_blocks += [ResidualBlock(base_channels)]
+
+        res_blocks += [
+            nn.Conv2d(base_channels, base_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(base_channels),
+        ]
+        self.res_blocks = nn.Sequential(*res_blocks)
+
+        # PixelShuffle blocks
+        ps_blocks = []
+        for _ in range(n_ps_blocks):
+            ps_blocks += [
+                nn.Conv2d(base_channels, 4 * base_channels, kernel_size=3, padding=1),
+                nn.PixelShuffle(2),
+                nn.PReLU(),
+            ]
+        self.ps_blocks = nn.Sequential(*ps_blocks)
+
+        # Output layer
+        self.out_layer = nn.Sequential(
+            nn.Conv2d(base_channels, 3, kernel_size=9, padding=4),
+            nn.Tanh(),
+        )
+
+    def forward(self, x):
+        x_res = self.in_layer(x)
+        x = x_res + self.res_blocks(x_res)
+        x = self.ps_blocks(x)
+        x = self.out_layer(x)
+        return x
+
+class Discriminator(nn.Module):
+    '''
+    Discriminator Class
+    Values:
+        base_channels: number of channels in first convolutional layer, a scalar
+        n_blocks: number of convolutional blocks, a scalar
+    '''
+
+    def __init__(self, base_channels=64, n_blocks=3):
+        super().__init__()
+        self.blocks = [
+            nn.Conv2d(3, base_channels, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(base_channels, base_channels, kernel_size=3, padding=1, stride=2),
+            nn.BatchNorm2d(base_channels),
+            nn.LeakyReLU(0.2, inplace=True),
+        ]
+
+        cur_channels = base_channels
+        for i in range(n_blocks):
+            self.blocks += [
+                nn.Conv2d(cur_channels, 2 * cur_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(2 * cur_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+
+                nn.Conv2d(2 * cur_channels, 2 * cur_channels, kernel_size=3, padding=1, stride=2),
+                nn.BatchNorm2d(2 * cur_channels),
+                nn.LeakyReLU(0.2, inplace=True),
+            ]
+            cur_channels *= 2
+
+        self.blocks += [
+            # You can replicate nn.Linear with pointwise nn.Conv2d
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(cur_channels, 2 * cur_channels, kernel_size=1, padding=0),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(2 * cur_channels, 1, kernel_size=1, padding=0),
+
+            # Apply sigmoid if necessary in loss function for stability
+            nn.Flatten(),
+        ]
+
+        self.layers = nn.Sequential(*self.blocks)
+
+    def forward(self, x):
+        return self.layers(x)
+
+class Loss(nn.Module):
+    '''
+    Loss Class
+    Implements composite content+adversarial loss for SRGAN
+    Values:
+        device: 'cuda' or 'cpu' hardware to put VGG network on, a string
+    '''
+
+    def __init__(self, device='cuda'):
+        super().__init__()
+
+        vgg = vgg19(pretrained=True).to(device)
+        self.vgg = nn.Sequential(*list(vgg.features)[:-1]).eval()
+        for p in self.vgg.parameters():
+            p.requires_grad = False
+
+    @staticmethod
+    def img_loss(x_real, x_fake):
+        return F.mse_loss(x_real, x_fake)
+
+    def adv_loss(self, x, is_real):
+        target = torch.zeros_like(x) if is_real else torch.ones_like(x)
+        return F.binary_cross_entropy_with_logits(x, target)
+
+    def vgg_loss(self, x_real, x_fake):
+        return F.mse_loss(self.vgg(x_real), self.vgg(x_fake))
+
+    def forward(self, generator, discriminator, hr_real, lr_real):
+        ''' Performs forward pass and returns total losses for G and D '''
+        hr_fake = generator(lr_real)
+        fake_preds_for_g = discriminator(hr_fake)
+        fake_preds_for_d = discriminator(hr_fake.detach())
+        real_preds_for_d = discriminator(hr_real.detach())
+
+        g_loss = (
+            0.001 * self.adv_loss(fake_preds_for_g, False) + \
+            0.006 * self.vgg_loss(hr_real, hr_fake) + \
+            self.img_loss(hr_real, hr_fake)
+        )
+        d_loss = 0.5 * (
+            self.adv_loss(real_preds_for_d, True) + \
+            self.adv_loss(fake_preds_for_d, False)
+        )
+
+        return g_loss, d_loss, hr_fake
+
+"""## Training SRGAN
+"""
+
+# We are using STL (for speed and also since ImageNet is no longer publicly available)
+USING_STL = True
+if USING_STL:
+    DatasetSubclass = torchvision.datasets.STL10
+else:
+    DatasetSubclass = torchvision.datasets.ImageNet
+
+class Dataset(DatasetSubclass):
+    '''
+    Dataset Class
+    Implements a general dataset class for STL10 and ImageNet
+    Values:
+        hr_size: spatial size of high-resolution image, a list/tuple
+        lr_size: spatial size of low-resolution image, a list/tuple
+        *args/**kwargs: all other arguments for subclassed torchvision dataset
+    '''
+
+    def __init__(self, *args, **kwargs):
+        hr_size = kwargs.pop('hr_size', [96, 96])
+        lr_size = kwargs.pop('lr_size', [24, 24])
+        super().__init__(*args, **kwargs)
+
+        if hr_size is not None and lr_size is not None:
+            assert hr_size[0] == 4 * lr_size[0]
+            assert hr_size[1] == 4 * lr_size[1]
+
+        # High-res images are cropped and scaled to [-1, 1]
+        self.hr_transforms = transforms.Compose([
+            transforms.RandomCrop(hr_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.Lambda(lambda img: np.array(img)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+        ])
+
+        # Low-res images are downsampled with bicubic kernel and scaled to [0, 1]
+        self.lr_transforms = transforms.Compose([
+            transforms.Normalize((-1.0, -1.0, -1.0), (2.0, 2.0, 2.0)),
+            transforms.ToPILImage(),
+            transforms.Resize(lr_size, interpolation=Image.BICUBIC),
+            transforms.ToTensor(),
+        ])
+
+        self.to_pil = transforms.ToPILImage()
+        self.to_tensor = transforms.ToTensor()
+
+    def __getitem__(self, idx):
+        # Uncomment the following lines if you're using ImageNet
+        # path, label = self.imgs[idx]
+        # image = Image.open(path).convert('RGB')
+
+        # Uncomment the following if you're using STL
+        image = torch.from_numpy(self.data[idx])
+        image = self.to_pil(image)
+
+        hr = self.hr_transforms(image)
+        lr = self.lr_transforms(hr)
+        return hr, lr
+
+    @staticmethod
+    def collate_fn(batch):
+        hrs, lrs = [], []
+
+        for hr, lr in batch:
+            hrs.append(hr)
+            lrs.append(lr)
+
+        return torch.stack(hrs, dim=0), torch.stack(lrs, dim=0)
+
+"""Recall that the generator (SRResNet) is first trained alone with MSE loss and is combined with the discriminator and trained as SRGAN after. Check out the training loops below:"""
+
+# Parse torch version for autocast
+# ######################################################
+version = torch.__version__
+version = tuple(int(n) for n in version.split('.')[:-1])
+has_autocast = version >= (1, 6)
+# ######################################################
+
+def show_tensor_images(image_tensor):
+    '''
+    Function for visualizing images: Given a tensor of images, number of images, and
+    size per image, plots and prints the images in an uniform grid.
+    '''
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:4], nrow=4)
+    plt.axis('off')
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+def train_srresnet(srresnet, dataloader, device, lr=1e-4, total_steps=1e6, display_step=500):
+    srresnet = srresnet.to(device).train()
+    optimizer = torch.optim.Adam(srresnet.parameters(), lr=lr)
+
+    cur_step = 0
+    mean_loss = 0.0
+    while cur_step < total_steps:
+        for hr_real, lr_real in tqdm(dataloader, position=0):
+            hr_real = hr_real.to(device)
+            lr_real = lr_real.to(device)
+
+            # Enable autocast to FP16 tensors (new feature since torch==1.6.0)
+            # If you're running older versions of torch, comment this out
+            # and use NVIDIA apex for mixed/half precision training
+            if has_autocast:
+                with torch.cuda.amp.autocast(enabled=(device=='cuda')):
+                    hr_fake = srresnet(lr_real)
+                    loss = Loss.img_loss(hr_real, hr_fake)
+            else:
+                hr_fake = srresnet(lr_real)
+                loss = Loss.img_loss(hr_real, hr_fake)
+            
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            mean_loss += loss.item() / display_step
+
+            if cur_step % display_step == 0 and cur_step > 0:
+                print('Step {}: SRResNet loss: {:.5f}'.format(cur_step, mean_loss))
+                show_tensor_images(lr_real * 2 - 1)
+                show_tensor_images(hr_fake.to(hr_real.dtype))
+                show_tensor_images(hr_real)
+                mean_loss = 0.0
+
+            cur_step += 1
+            if cur_step == total_steps:
+                break
+
+def train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=500):
+    generator = generator.to(device).train()
+    discriminator = discriminator.to(device).train()
+    loss_fn = Loss(device=device)
+
+    g_optimizer = torch.optim.Adam(generator.parameters(), lr=lr)
+    d_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr)
+    g_scheduler = torch.optim.lr_scheduler.LambdaLR(g_optimizer, lambda _: 0.1)
+    d_scheduler = torch.optim.lr_scheduler.LambdaLR(d_optimizer, lambda _: 0.1)
+
+    lr_step = total_steps // 2
+    cur_step = 0
+
+    mean_g_loss = 0.0
+    mean_d_loss = 0.0
+
+    while cur_step < total_steps:
+        for hr_real, lr_real in tqdm(dataloader, position=0):
+            hr_real = hr_real.to(device)
+            lr_real = lr_real.to(device)
+
+            # Enable autocast to FP16 tensors (new feature since torch==1.6.0)
+            # If you're running older versions of torch, comment this out
+            # and use NVIDIA apex for mixed/half precision training
+            if has_autocast:
+                with torch.cuda.amp.autocast(enabled=(device=='cuda')):
+                    g_loss, d_loss, hr_fake = loss_fn(
+                        generator, discriminator, hr_real, lr_real,
+                    )
+            else:
+                g_loss, d_loss, hr_fake = loss_fn(
+                    generator, discriminator, hr_real, lr_real,
+                )
+
+            g_optimizer.zero_grad()
+            g_loss.backward()
+            g_optimizer.step()
+
+            d_optimizer.zero_grad()
+            d_loss.backward()
+            d_optimizer.step()
+
+            mean_g_loss += g_loss.item() / display_step
+            mean_d_loss += d_loss.item() / display_step
+
+            if cur_step == lr_step:
+                g_scheduler.step()
+                d_scheduler.step()
+                print('Decayed learning rate by 10x.')
+
+            if cur_step % display_step == 0 and cur_step > 0:
+                print('Step {}: Generator loss: {:.5f}, Discriminator loss: {:.5f}'.format(cur_step, mean_g_loss, mean_d_loss))
+                show_tensor_images(lr_real * 2 - 1)
+                show_tensor_images(hr_fake.to(hr_real.dtype))
+                show_tensor_images(hr_real)
+                mean_g_loss = 0.0
+                mean_d_loss = 0.0
+
+            cur_step += 1
+            if cur_step == total_steps:
+                break
+
+"""Now initialize everything and run training!"""
+
+#device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#generator = Generator(n_res_blocks=16, n_ps_blocks=2)
+
+# Uncomment the following lines if you're using ImageNet
+# dataloader = torch.utils.data.DataLoader(
+#     Dataset('data', 'train', download=True, hr_size=[384, 384], lr_size=[96, 96]),
+#     batch_size=16, pin_memory=True, shuffle=True,
+# )
+# train_srresnet(generator, dataloader, device, lr=1e-4, total_steps=1e6, display_step=500)
+# torch.save(generator, 'srresnet.pt')
+
+# Uncomment the following lines if you're using STL
+#dataloader = torch.utils.data.DataLoader(
+#    Dataset('data', 'train', download=True, hr_size=[96, 96], lr_size=[24, 24]),
+#    batch_size=16, pin_memory=True, shuffle=True,
+#)
+#train_srresnet(generator, dataloader, device, lr=1e-4, total_steps=1e5, display_step=1000)
+#torch.save(generator, 'srresnet.pt')
+
+#generator = torch.load('srresnet.pt')
+#discriminator = Discriminator(n_blocks=1, base_channels=8)
+
+# Uncomment the following lines if you're using ImageNet
+# train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=500)
+# torch.save(generator, 'srgenerator.pt')
+# torch.save(discriminator, 'srdiscriminator.pt')
+
+# Uncomment the following lines if you're using STL
+#train_srgan(generator, discriminator, dataloader, device, lr=1e-4, total_steps=2e5, display_step=1000)
+#torch.save(generator, 'srgenerator2.pt')
+#torch.save(discriminator, 'srdiscriminator2.pt')
+
+
+
+
+#srgan = torch.load('srgenerator.pt')
+#img_hres = srgan(img)
+
+#transforms.ToPILImage(img_hres)
+
+#transforms.ToTensor(np.array(img_hres))
